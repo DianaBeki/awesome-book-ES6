14c127485c76f8d9109e83fda8172e02e479bc2c
DianaBeki <dianabeki54@gmail.com>
2023-06-05 22:55:40 +0300

    Select a class

--- modules/bookDisplay.js
+++ modules/bookDisplay.js
@@ -12,7 +12,6 @@ const displayBooks = ()=>{
         const button = document.createElement('button');
         button.classList.add('removeBtn');
         button.textContent = 'Remove';
-        button.style.
      
         div.appendChild(paragraph);
         div.appendChild(button);
