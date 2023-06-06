bd850234015e189cfd08a40a08876c6bc45e9020
DianaBeki <dianabeki54@gmail.com>
2023-06-06 12:59:58 +0300

    Update files

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import display from "./modules/bookDisplay.js";
-import add from './modules/add.js'
+import Display from "./modules/bookDisplay.js";
 import nav from "./modules/Navbar.js";
 import  "./modules/date.js";
-const addForm = document.getElementById('addForm');
+
 nav();
-display();
-    addForm.addEventListener('submit', (e) => {
-        // e.preventDefault();
-        add(title.value, author.value);
-        addForm.reset();
-      });
+const display = new Display();
+display.display();
+display.init();
