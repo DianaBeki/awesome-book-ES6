d06a057fc7202424fdffeffab017ab661b88cb15
DianaBeki <dianabeki54@gmail.com>
2023-06-06 15:17:56 +0300

    Update files

--- modules/bookDisplay.js
+++ modules/bookDisplay.js
@@ -1,24 +1,29 @@
-class Display{
-  constructor(){
-      this.bookShop = JSON.parse(localStorage.getItem('bookShop')) || [];
-      this.bookList = document.querySelector('.bookList');
-      this.addForm = document.getElementById('addForm');
+class Display {
+  constructor() {
+    this.bookShop = JSON.parse(localStorage.getItem('bookShop')) || [];
+    this.bookList = document.querySelector('.bookList');
+    this.addForm = document.getElementById('addForm');
+    this.userTitle = document.getElementById('title');
+    this.userAuthor = document.getElementById('author');
   }
+
   add(title, author) {
-      this.bookShop.push({ title, author });
-      localStorage.setItem('bookShop', JSON.stringify(this.bookShop));
-    }
-    remove(title, author) {
-      this.bookShop = this.bookShop.filter((b) => b.title !== title || b.author !== author);
-      localStorage.setItem('bookShop', JSON.stringify(this.bookShop));
-    }
-  display(){
-      this.bookList.innerHTML = '';
-  for(let i = 0; i < this.bookShop.length; i++){
+    this.bookShop.push({ title, author });
+    localStorage.setItem('bookShop', JSON.stringify(this.bookShop));
+  }
+
+  remove(title, author) {
+    this.bookShop = this.bookShop.filter((b) => b.title !== title || b.author !== author);
+    localStorage.setItem('bookShop', JSON.stringify(this.bookShop));
+  }
+
+  display() {
+    this.bookList.innerHTML = '';
+    this.bookShop.forEach((item) => {
       const div = document.createElement('div');
       div.classList.add('bookwrapper');
       const paragraph = document.createElement('p');
-      paragraph.textContent = `"${this.bookShop[i].title}", ${this.bookShop[i].author}`;
+      paragraph.textContent = `"${item.title}", ${item.author}`;
       const buttonRemove = document.createElement('button');
       buttonRemove.classList.add('removeBtn');
       buttonRemove.textContent = 'Remove';
@@ -26,18 +31,19 @@ class Display{
       div.appendChild(buttonRemove);
       this.bookList.appendChild(div);
       buttonRemove.addEventListener('click', () => {
-          this.remove(this.bookShop[i].title, this.bookShop[i].author);
-          this.display()
-        });
-   }
+        this.remove(item.title, item.author);
+        this.display();
+      });
+    });
   }
-  init(){
-      this.addForm.addEventListener('submit', (e) => {
-          e.preventDefault();
-          this.add(title.value, author.value);
-          this.display();
-          this.addForm.reset();
-        });
+
+  init() {
+    this.addForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.add(this.userTitle.value, this.userAuthor.value);
+      this.display();
+      this.addForm.reset();
+    });
   }
 }
 export default Display;
